8ef49cbcf09409b2cd51922ca4c9aa056ff7c7f1
Joshfishman97 <fishmanjosh251@gmail.com>
2021-07-02 00:16:52 +0000

    more updates

--- exercises/20.2-More-mapping/app.js
+++ exercises/20.2-More-mapping/app.js
@@ -1,12 +1,9 @@
 
 
 var myNumbers = [23,234,345,4356234,243,43,56,2];
-
-var newArray = myNumbers.map(function myFunction(item, index,arr){
+function myFunction(item, index,arr){
     return item * 3;
-});
-function myFunction(){
-    for(let i =0;  i<newArray.length; i++)
-    console.log(newArray.length[i])
 }
+var newArray = myNumbers.map(myFunction);
+
 console.log(newArray);
--- exercises/20.3-Map-with-function-inside-variable/app.js
+++ exercises/20.3-Map-with-function-inside-variable/app.js
@@ -3,7 +3,7 @@ let names = ['Alice','Bob','Marry','Joe','Hilary','Stevia','Dylan'];
 let prepender = function(name){
 	return 'My name is: '+name;
 };
-let a = names.map(function prepender(name){
+let answer = names.map(function prepender(name){
     return 'My name is: '+name;
 })
-console.log(a)
+console.log(answer)
